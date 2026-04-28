e3382e4e4eb248f8edd0e78338ad5f4d5848074a
Alex210185 <ale.santos_85@outlook.com>
2026-04-28 15:44:33 -0300

    Ajust Main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,78 +1,55 @@
-# main_spark.py
 import logging
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, year, month
-#spark = SparkSession.builder \
- #   .appName("Airflow-S3-Pipeline") \
-  #  .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-   # .config("spark.hadoop.fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A") \
-    #.config("spark.hadoop.fs.s3a.access.key", "YOUR_ACCESS_KEY") \
-    #.config("spark.hadoop.fs.s3a.secret.key", "YOUR_SECRET_KEY") \
-    #.config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
-    #.getOrCreate()
-spark = SparkSession.builder \
-    .appName("Airflow-S3-Pipeline") \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .config("spark.hadoop.fs.s3a.access.key", "YOUR_ACCESS_KEY") \
-    .config("spark.hadoop.fs.s3a.secret.key", "YOUR_SECRET_KEY") \
-    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
-    .getOrCreate()
-
-    
-# Configure logging
+from extraction.extract import read_csv_from_s3
+from extraction.validate import validate_customers, validate_items
+from s3_utils import write_dicts_to_s3_parquet
+from extraction.extract import read_csv_from_s3, list_bucket_objects
+from s3_utils import write_dicts_to_s3_parquet, write_items_partitioned
+# Configure logging for this module
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-logger = logging.getLogger("pipeline_spark")
+logger = logging.getLogger("pipeline")
 
 def run_pipeline():
-    # Initialize Spark
-    spark = SparkSession.builder \
-        .appName("Airflow-S3-Pipeline") \
-        .config("spark.sql.shuffle.partitions", "8") \
-        .getOrCreate()
+    
+    source_bucket = "source-bucket-230525"
+    target_bucket = "target-bucket-230525"
 
-    source_bucket = "s3a://source-bucket-230525"
-    target_bucket = "s3a://target-bucket-230525"
-
-    logger.info("🚀 Starting Spark pipeline execution")
+    logger.info("🚀 Starting pipeline execution")
     logger.info(f"Source bucket: {source_bucket} | Target bucket: {target_bucket}")
 
+ # Debug: list files before extraction
+    logger.info("Listing files in source bucket...")
+    list_bucket_objects(source_bucket)
     # Step 1: Extract
     logger.info("Step 1: Extracting data from S3")
-    customers_df = spark.read.csv(f"{source_bucket}/customers.csv", header=True, inferSchema=True)
-    items_df = spark.read.csv(f"{source_bucket}/Items.csv", header=True, sep=";", inferSchema=True)
-
-    logger.info(f"Customers extracted: {customers_df.count()} rows")
-    logger.info(f"Items extracted: {items_df.count()} rows")
+    customers_df = read_csv_from_s3(source_bucket, "customers.csv", delimiter=",")
+    items_df = read_csv_from_s3(source_bucket, "Items.csv", delimiter=";")
+    
+    logger.info(f"Customers extracted: {len(customers_df)} rows")
+    logger.info(f"Items extracted: {len(items_df)} rows")
 
     # Step 2: Validate
     logger.info("Step 2: Validating data")
-    valid_customers = customers_df.filter(col("State") == "CA").cache()
-    invalid_customers = customers_df.filter(col("State") != "CA")
+    valid_customers, invalid_customers = validate_customers(customers_df)
+    valid_items, invalid_items = validate_items(items_df)
 
-    valid_items = items_df.filter(col("CustomerID").isNotNull()).cache()
-    invalid_items = items_df.filter(col("CustomerID").isNull())
-
-    logger.info(f"Valid customers: {valid_customers.count()} | Invalid customers: {invalid_customers.count()}")
-    logger.info(f"Valid items: {valid_items.count()} | Invalid items: {invalid_items.count()}")
+    logger.info(f"Valid customers: {len(valid_customers)} | Invalid customers: {len(invalid_customers)}")
+    logger.info(f"Valid items: {len(valid_items)} | Invalid items: {len(invalid_items)}")
 
     # Step 3: Load
     logger.info("Step 3: Writing validated data to S3")
+    try:
+        # Partition items by Satates
+        write_dicts_to_s3_parquet(valid_customers.to_dict(orient="records"),target_bucket, "customers/valid")
+        write_dicts_to_s3_parquet(invalid_customers.to_dict(orient="records"),target_bucket, "customers/invalid")
+        
+        # Partition items by Year/Month from Purchase Date
+        write_items_partitioned(valid_items.to_dict(orient="records"),target_bucket, "items/valid")
+        write_items_partitioned(invalid_items.to_dict(orient="records"),target_bucket, "items/invalid")
 
-    # Customers
-    valid_customers.write.mode("overwrite").parquet(f"{target_bucket}/customers/valid")
-    invalid_customers.write.mode("overwrite").parquet(f"{target_bucket}/customers/invalid")
+    except Exception as e:
+        logger.exception(f"Error during load step: {e}")
 
-    # Items with partitioning by Year/Month
-    items_with_date = valid_items.withColumn("Year", year(col("PurchaseDate"))) \
-                                 .withColumn("Month", month(col("PurchaseDate")))
-
-    items_with_date.write.partitionBy("Year", "Month").mode("overwrite").parquet(f"{target_bucket}/items/valid")
-
-    invalid_items.withColumn("Year", year(col("PurchaseDate"))) \
-                 .withColumn("Month", month(col("PurchaseDate"))) \
-                 .write.partitionBy("Year", "Month").mode("overwrite").parquet(f"{target_bucket}/items/invalid")
-
-    logger.info("✅ Spark pipeline completed successfully!")
+    print("✅ Pipeline completed successfully!")
 
 if __name__ == "__main__":
     run_pipeline()
